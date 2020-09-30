522b05612a477db10d1a043816a339123dd506b7
Meng Wu <tg826544@login3.stampede2.tacc.utexas.edu>
2020-09-30 01:29:04 -0500

    rename geteqpdat scripts

--- epsmerge.sh
+++ epsmerge.sh
@@ -1,87 +1,25 @@
 #!/bin/bash
-#This script merge eps0mat.h5 and epsmat.h5 files
-###############################################################
-#Default: 1 to end
-if [ $# -gt 3 ]; then
-   echo "USAGE: pw2batch.sh [SUBFLAG:1=true,else=false] [StartIndex] [EndIndex] "
-   exit 0
-fi
+#SBATCH --job-name=MERGE
+#SBATCH --partition=skx-dev
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH --time=02:00:00
+#SBATCH -A TG-MCA94P030
 
-echo "Number of kpoints file: $(sed -n '2 p' qptlib/qgrid.log | awk '{print $5}') "
+#module unload phdf5
+#module unload impi
+#module load hdf5
 
-if [ $# -eq 0 ];then
-   echo "You are using the default setting:"
-   SUBFLAG=1
-   StartIndex=1
-   EndIndex=$(sed -n '2 p' qptlib/qgrid.log | awk '{print $5}')
-   echo "We are submitting jobs from $StartIndex to $EndIndex"
-else
-   if [ $# -eq 1 ];then
-   SUBFLAG=$1
-   StartIndex=1
-   EndIndex=$(sed -n '2 p' qptlib/qgrid.log | awk '{print $5}')
-   echo "We are submitting jobs from $StartIndex to $EndIndex"
-   else
-      if [ $# -eq 2 ]; then
-	 SUBFLAG=$1
-         StartIndex=$2
-         EndIndex=$(sed -n '2 p' qptlib/qgrid.log | awk '{print $5}')
-         echo "We are submitting jobs from $StartIndex to $EndIndex" 
-      else
-	 if [ $# -eq 3 ]; then
-	    SUBFLAG=$1
-            StartIndex=$2
-            EndIndex=$3
-            echo "We are submitting jobs from $StartIndex to $EndIndex" 
-	 fi
-      fi
-   fi
-fi
-numoffiles=$(echo "$EndIndex-$StartIndex+1" | bc )
+# This script calls epsmat_hdf5_merge.py to merge all the chimat_eh.[x].h5 files
+# Number of q1 vectors
+q_start=1
+q_end=6
+for ((iq=${q_start};iq<=${q_end};iq++))
+do
+    DIRNAME="Q${iq}"
+    ln -sf ../${DIRNAME}/eps0mat.h5 ./eps0mat.${iq}.h5
+done
 
-rm -rf merge_q0_all
-rm -rf merge_q_all
-mkdir merge_q0_all
-mkdir merge_q_all
-q0counter=1
-qcounter=1
-for ((i=$StartIndex;i<=$EndIndex;i++))
-do
-  cd P_${i}
-  if [ -f eps0mat.h5 ];then
-     echo "Found eps0mat in P_${i}. Linking it to directory merge_q0_all ..."
-     ln -s ../P_${i}/eps0mat.h5 ../merge_q0_all/eps0mat.${q0counter}.h5
-     q0counter=$(echo "$q0counter+1" | bc ) 
-  fi
-  
-  if [ -f epsmat.h5 ];then
-     echo "Found epsmat in P_${i}. Linking it to directory merge_q_all ..."
-     ln -s ../P_${i}/epsmat.h5 ../merge_q_all/epsmat.${qcounter}.h5
-     qcounter=$(echo "$qcounter+1" | bc )
-  fi
-  cd ..
-done
-###################################################
-#Merge eps0mat
-cd merge_q0_all
-q0flag=" -o eps0mat.h5"
-for ((i=1;i<$q0counter;i++))
-do
-    q0flag="${q0flag} eps0mat.$i.h5"
-done
-echo "q0Flag: $q0flag"
-epsmat_hdf5_merge.py $q0flag
-cd ..
-ln -s -f merge_q0_all/eps0mat.h5 .
-###################################################
-#Merge epsmat
-cd merge_q_all
-qflag=" -o epsmat.h5"
-for ((i=1;i<$qcounter;i++))
-do
-    qflag="${qflag} epsmat.$i.h5"
-done
-echo "qFlag: $qflag"
-epsmat_hdf5_merge.py $qflag
-cd ..
-ln -s -f merge_q_all/epsmat.h5 .
+filestring=$(seq -s " " -f "eps0mat.%g.h5" ${q_start} ${q_end})
+echo ${filestring}
+~/bin/btools/epsmat_hdf5_merge_V2.py -o eps0mat.h5 ${filestring} > log
