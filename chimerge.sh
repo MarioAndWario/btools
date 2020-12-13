2f564d2e9af4d0c1a6cb03e12eb10cf0652d005a
wu2meng3 <wu1meng2@berkeley.edu>
2020-12-12 17:31:30 -0800

    add getchi00.sh

--- chimerge.sh
+++ chimerge.sh
@@ -1,10 +1,20 @@
-#!/bin/bash
+#!/bin/bash -l
+
+#SBATCH --partition=debug
+#SBATCH --nodes=1
+#SBATCH --time=00:30:00
+#SBATCH --job-name=MW
+#SBATCH -C haswell
+
+#export OMP_NUM_THREADS=1
 
 # This script calls epsmat_hdf5_merge.py to merge all the chimat_eh.[x].h5 files
 
 # Number of q1 vectors
 nq1_start=1
-nq1_end=28
+nq1_end=29
 filestring=$(seq -s " " -f "chimat_eh.%g.h5" ${nq1_start} ${nq1_end})
 
-python epsmat_hdf5_merge.py -o chimat_eh.h5 ${filestring}
+echo ${filestring}
+
+python ~/bin/btools/epsmat_hdf5_merge_V2.py -o chimat_eh.h5 ${filestring}
--- getepsinvhead2.sh
+++ getepsinvhead2.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
-file="eps.out"
+if [ $# -gt 0 ]; then
+    file=$1
+else
+    file="ce1.out"
+fi
 
 nrealfreq=$(grep 'Real-axis frequencies' ${file} | awk '{print $5}')
 nimagfreq=$(grep 'Imaginary-axis frequencies' ${file} | awk '{print $5}')
 
-freqstartline=$(grep -n 'Finished inversion of dielectric matrix' ${file} | head -n 1 | awk -F ":" '{print $1+3}')
-#echo "$freqstartline"
+freqstartline=$(grep -n 'Epsinv(G=0' ${file} | head -n 1 | awk -F ":" '{print $1}')
+echo "$freqstartline"
 
 if [ -z $nrealfreq ]; then
     echo "No real-axis frequencies."
     nrealfreq=0
 else
     realfreqendline=$(echo "$freqstartline+$nrealfreq-1" | bc)
-    #echo "$realfreqendline"
+    echo "$realfreqendline"
     echo "Outputing $nrealfreq epsinv at real-axis frequencies to epsinvhead2_real.txt"
     sed -n "${freqstartline}, ${realfreqendline} p" ${file} | awk '{printf("   %22.15e   %22.15e\n", $7,$8)}' > epsinvhead2_real.txt    
 fi
@@ -22,9 +26,9 @@ if [ -z $nimagfreq ]; then
     nimagfreq=0
 else
     imagfreqstartline=$(echo "$freqstartline+$nrealfreq" | bc)
-    #echo "$imagfreqstartline"
+    echo "$imagfreqstartline"
     imagfreqendline=$(echo "$imagfreqstartline+$nimagfreq-1" | bc)
-    #echo "$imagfreqendline"
+    echo "$imagfreqendline"
 
     echo "Outputing $nimagfreq epsinv at imaginary-axis frequencies to epsinvhead2_imag.txt"    
     sed -n "${imagfreqstartline}, ${imagfreqendline} p" ${file} | awk '{printf("   %22.15e   %22.15e\n", $7,$8)}' > epsinvhead2_imag.txt    
